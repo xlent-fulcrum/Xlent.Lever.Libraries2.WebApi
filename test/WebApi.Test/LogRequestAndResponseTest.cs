fc039c09fd460e7eaadbb9ba5f5c9b159745f9fa
magnuswiderberg <magnus.widerberg@xlent.se>
2017-08-16 22:15:12 +0200

    Downgrade Microsoft.CodeDom.Providers.DotNetCompilerPlatform from 1.0.6 to 1.0.5 because of problems with deploy in Azure

---
+++ test/WebApi.Test/LogRequestAndResponseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Xlent.Lever.Libraries2.Core.Logging.Model;
+using Xlent.Lever.Libraries2.WebApi.Pipe.Inbound;
+
+namespace Xlent.Lever.Libraries2.WebApi.Test
+{
+    [TestClass]
+    public class LogRequestAndResponseTest
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var logger = new Mock<IFulcrumLogger>();
+            logger.Setup(x => x.LogAsync(It.IsAny<LogSeverityLevel>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask)
+                .Callback<LogSeverityLevel, string>(
+                    (level, message) =>
+                    {
+                        
+                    }
+                );
+
+            var logRequestAndResponse = new LogRequestAndResponse(logger.Object);
+            // TODO: How to test?
+        }
+    }
+}
