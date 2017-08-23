2e0b7e96cd0364a46ab5a2d1189d23d5244e5364
magnuswiderberg <magnus.widerberg@xlent.se>
2017-08-23 09:17:47 +0200

    2.3.0 Added SaveValuesProvider which works with a HttpContextValueProvider, because the AsyncLocalValueProvider does not work fully

---
+++ src/Libraries2.WebApi/Context/HttpContextValueProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using Xlent.Lever.Libraries2.Core.Assert;
+using Xlent.Lever.Libraries2.Core.Context;
+
+namespace Xlent.Lever.Libraries2.WebApi.Context
+{
+    /// <summary>
+    /// Stores values in the execution context which is unaffected by asynchronous code that switches threads or context. 
+    /// </summary>
+    /// <remarks>Updating values in a thread will not affect the value in parent/sibling threads</remarks>
+    public class HttpContextValueProvider : IValueProvider
+    {
+        /// <inheritdoc />
+        public T GetValue<T>(string key)
+        {
+            InternalContract.RequireNotNullOrWhitespace(key, nameof(key));
+            if (HttpContext.Current.Items.Contains(key) != true) return default(T);
+            var value = HttpContext.Current.Items[key];
+            return (T)value;
+        }
+
+        /// <inheritdoc />
+        public void SetValue<T>(string key, T value)
+        {
+            InternalContract.RequireNotNullOrWhitespace(key, nameof(key));
+            InternalContract.RequireNotNull(value, nameof(value));
+            HttpContext.Current.Items.Add(key, value);
+        }
+    }
+}
