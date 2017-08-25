7b9b8be233bb3af913e37fb7adb4e2e347553924
magnuswiderberg <magnus.widerberg@xlent.se>
2017-08-25 10:45:02 +0200

    3.0.2 Convenience for choosing the right IValueProvider.

---
+++ src/Libraries2.WebApi/Context/ContextValueProvider.cs
@@ -0,0 +1,20 @@
+﻿using Xlent.Lever.Libraries2.Core.Context;
+
+namespace Xlent.Lever.Libraries2.WebApi.Context
+{
+    /// <summary>
+    /// Convenience for choosing the right <see cref="IValueProvider"/>.
+    /// </summary>
+    public class ContextValueProvider : Core.Context.ContextValueProvider
+    {
+        static ContextValueProvider()
+        {
+            Chosen = RecommendedForWebApi;
+        }
+
+        /// <summary>
+        /// Default <see cref="IValueProvider"/> for .Net Framework Web Api.
+        /// </summary>
+        public static IValueProvider RecommendedForWebApi { get; } = new HttpContextValueProvider();
+    }
+}
