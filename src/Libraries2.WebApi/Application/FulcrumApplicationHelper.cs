5e3e65707f62aae1ce17492ca4dba302628e80f1
Lars Lindberg <lars.lindberg@xlent.se>
2017-09-02 16:41:26 +0200

    4.1.4 Better AppSettings.

--- src/Libraries2.WebApi/Application/FulcrumApplicationHelper.cs
+++ src/Libraries2.WebApi/Application/FulcrumApplicationHelper.cs
@@ -19,28 +19,29 @@ namespace Xlent.Lever.Libraries2.WebApi.Application
         /// <param name="name">The name of the application.</param>
         /// <param name="tenant">The tenant that the application itself runs in.</param>
         /// <param name="level">The run time level for the application itself.</param>
-        /// <param name="logger">An optional logger to use instead of the default logger.</param>
-        public static void WebApiBasicSetup(string name, ITenant tenant, RunTimeLevelEnum level, IFulcrumLogger logger = null)
+        public static void WebApiBasicSetup(string name, ITenant tenant, RunTimeLevelEnum level)
         {
+            FulcrumApplication.Initialize(name, tenant, level);
             FulcrumApplication.Setup.ThreadHandler = new HostingThreads();
-            FulcrumApplication.Setup.Logger = logger ?? Log.RecommendedForNetFramework;
+            FulcrumApplication.Setup.Logger = Log.RecommendedForNetFramework;
             FulcrumApplication.Setup.ContextValueProvider = ContextValueProvider.RecommendedForWebApi;
+            FulcrumApplication.AppSettings = new AppSettings(new ConfigurationManagerAppSettings());
         }
 
         /// <summary>
         /// Sets the recommended application setup for a Web Api.
         /// </summary>
         /// <param name="appSettingGetter">How to get app settings for <see cref="ApplicationSetup.Name"/>, <see cref="ApplicationSetup.Tenant"/>, <see cref="ApplicationSetup.RunTimeLevel"/></param>
-        /// <param name="logger">An optional logger to use instead of the default logger.</param>
         /// <remarks>If you want to use <see cref="ConfigurationManager"/> for retreiving app settings, you can use <see cref="ConfigurationManagerAppSettings"/> as the <paramref name="appSettingGetter"/>.</remarks>
-        public static void WebApiBasicSetup(IAppSettingGetter appSettingGetter, IFulcrumLogger logger = null)
+        public static void WebApiBasicSetup(IAppSettingGetter appSettingGetter)
         {
             InternalContract.RequireNotNull(appSettingGetter, nameof(appSettingGetter));
             var appSettings = new AppSettings(appSettingGetter);
             var name = appSettings.GetString("ApplicationName", true);
             var tenant = appSettings.GetTenant("Organization", "Environment", true);
             var runTimeLevel = appSettings.GetEnum<RunTimeLevelEnum>("RunTimeLevel", true);
-            WebApiBasicSetup(name, tenant, runTimeLevel, logger);
+            WebApiBasicSetup(name, tenant, runTimeLevel);
+            FulcrumApplication.AppSettings = new AppSettings(appSettingGetter);
         }
     }
 }
