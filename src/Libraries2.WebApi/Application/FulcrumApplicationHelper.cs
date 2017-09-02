265348d6b25b305ee27ba89e09a8decf7ae67c02
Lars Lindberg <lars.lindberg@xlent.se>
2017-09-02 10:58:01 +0200

    4.1.0 Introduced app settings support.

--- src/Libraries2.WebApi/Application/FulcrumApplicationHelper.cs
+++ src/Libraries2.WebApi/Application/FulcrumApplicationHelper.cs
@@ -1,4 +1,6 @@
-﻿using Xlent.Lever.Libraries2.Core.Application;
+﻿using System.Configuration;
+using Xlent.Lever.Libraries2.Core.Application;
+using Xlent.Lever.Libraries2.Core.Assert;
 using Xlent.Lever.Libraries2.Core.Logging.Logic;
 using Xlent.Lever.Libraries2.Core.MultiTenant.Model;
 using Xlent.Lever.Libraries2.WebApi.Context;
@@ -12,7 +14,7 @@ namespace Xlent.Lever.Libraries2.WebApi.Application
     public static class FulcrumApplicationHelper
     {
         /// <summary>
-        /// Sets the recommended application setup for .NET Framework.
+        /// Sets the recommended application setup for a Web Api.
         /// </summary>
         /// <param name="name">The name of the application.</param>
         /// <param name="tenant">The tenant that the application itself runs in.</param>
@@ -22,6 +24,21 @@ namespace Xlent.Lever.Libraries2.WebApi.Application
             FulcrumApplication.Setup.ThreadHandler = new HostingThreads();
             FulcrumApplication.Setup.Logger = Log.RecommendedForNetFramework;
             FulcrumApplication.Setup.ContextValueProvider = ContextValueProvider.RecommendedForWebApi;
+        }
+
+        /// <summary>
+        /// Sets the recommended application setup for a Web Api.
+        /// </summary>
+        /// <paramref name="appSettingGetter"/>How to get app settings for <see cref="ApplicationSetup.Name"/>, <see cref="ApplicationSetup.Tenant"/>, <see cref="ApplicationSetup.RunTimeLevel"/>
+        /// <remarks>If you want to use <see cref="ConfigurationManager"/> for retreiving app settings, you can use <see cref="ConfigurationManagerAppSettings"/> as the <paramref name="appSettingGetter"/>.</remarks>
+        public static void NetFrameworkSetup(IAppSettingGetter appSettingGetter)
+        {
+            InternalContract.RequireNotNull(appSettingGetter, nameof(appSettingGetter));
+            var appSettings = new AppSettings(appSettingGetter);
+            var name = appSettings.GetString("ApplicationName", true);
+            var tenant = appSettings.GetTenant("Organization", "Environment", true);
+            var runTimeLevel = appSettings.GetEnum<RunTimeLevelEnum>("RunTimeLevel", true);
+            WebApiBasicSetup(name, tenant, runTimeLevel);
         }
     }
 }
