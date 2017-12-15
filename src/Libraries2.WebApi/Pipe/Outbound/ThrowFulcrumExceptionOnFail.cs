44aba8ee04ad1fb3a125c881af5e939883b77aa1
Lars Lindberg <lars.lindberg@xlent.se>
2017-12-15 13:18:35 +0100

    Better exception handling in outbound pipe.

--- src/Libraries2.WebApi/Pipe/Outbound/ThrowFulcrumExceptionOnFail.cs
+++ src/Libraries2.WebApi/Pipe/Outbound/ThrowFulcrumExceptionOnFail.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Rest;
 using Xlent.Lever.Libraries2.Core.Error.Logic;
 using Xlent.Lever.Libraries2.Core.Logging;
 using Xlent.Lever.Libraries2.WebApi.Error.Logic;
@@ -16,11 +18,23 @@ namespace Xlent.Lever.Libraries2.WebApi.Pipe.Outbound
         /// <inheritdoc />
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var response = await base.SendAsync(request, cancellationToken);
-            var fulcrumException = await Converter.ToFulcrumExceptionAsync(response);
-            if (fulcrumException == null) return response;
-            Log.LogInformation($"OUT request-response {response.ToLogString()} was converted to a FulcrumException:\r{fulcrumException.ToLogString()}");
-            throw fulcrumException;
+            try
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+                var fulcrumException = await Converter.ToFulcrumExceptionAsync(response);
+                if (fulcrumException == null) return response;
+                Log.LogInformation(
+                    $"OUT request-response {response.ToLogString()} was converted to a FulcrumException:\r{fulcrumException.ToLogString()}");
+                throw fulcrumException;
+            }
+            catch (FulcrumException)
+            {
+                throw;
+            }
+            catch (Exception exception)
+            {
+                throw new FulcrumAssertionFailedException($"Request {request.ToLogString()} failed with exception of type {exception.GetType()}.", exception);
+            }
         }
     }
 }
