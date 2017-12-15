56bbeab55fb95f00a1909c447d14a50a4c34bf89
magnuswiderberg <magnus.widerberg@xlent.se>
2017-12-15 11:17:22 +0100

    4.3.14 Bug fix in ClientCredentials

---
+++ src/Libraries2.WebApi/Platform/ReleaseNotes/ReleaseNotesHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+using Newtonsoft.Json.Linq;
+using Xlent.Lever.Libraries2.Core.Assert;
+
+namespace Xlent.Lever.Libraries2.WebApi.Platform.ReleaseNotes
+{
+    /// <summary>
+    /// Provides release notes for the service.
+    /// </summary>
+    public class ReleaseNotesHelper
+    {
+        /// <summary>
+        /// Reads ReleaseNotes.json from App_Data and parses it.
+        /// </summary>
+        public static IEnumerable<ReleaseNotes> ReadReleaseNotes()
+        {
+            const string filePath = "~/App_Data/ReleaseNotes.json";
+            var file = HostingEnvironment.MapPath(filePath);
+            FulcrumAssert.IsNotNull(file, null, $"Expected file '{filePath}' to exist");
+            var text = File.ReadAllText(file ?? throw new ArgumentNullException());
+            var entries = JArray.Parse(text);
+            return entries.Select(x => x.ToObject<ReleaseNotes>());
+        }
+    }
+}
