353c30df9eac88e519847b13ff6c7672ee9bc37e
Lars Lindberg <lars.lindberg@xlent.se>
2018-05-09 18:49:23 +0200

    Added attributes for Swagger annotations.

---
+++ src/Libraries2.WebApi/Annotations/SwaggerBadRequestResponseAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Swashbuckle.Swagger.Annotations;
+using Xlent.Lever.Libraries2.Core.Error.Model;
+
+namespace Xlent.Lever.Libraries2.WebApi.Annotations
+{
+    /// <summary>
+    /// Information about a translation concept
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SwaggerBadRequestResponseAttribute : SwaggerResponseAttribute
+    {
+        /// <summary>
+        /// Create the swagger documentation for a failure with <see cref="HttpStatusCode.BadRequest"/> status code.
+        /// </summary>
+        public SwaggerBadRequestResponseAttribute()
+            : base(HttpStatusCode.BadRequest,
+                "Bad request. The service could not accept the request. See the body for mor information.", 
+                typeof(FulcrumError))
+        {
+        }
+    }
+}
---
+++ src/Libraries2.WebApi/Annotations/SwaggerInternalServerErrorResponseAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Swashbuckle.Swagger.Annotations;
+using Xlent.Lever.Libraries2.Core.Error.Model;
+
+namespace Xlent.Lever.Libraries2.WebApi.Annotations
+{
+    /// <summary>
+    /// Information about a translation concept
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SwaggerInternalServerErrorResponseAttribute : SwaggerResponseAttribute
+    {
+        /// <summary>
+        /// Create the swagger documentation for a failure with <see cref="HttpStatusCode.InternalServerError"/> status code.
+        /// </summary>
+        public SwaggerInternalServerErrorResponseAttribute()
+            : base(HttpStatusCode.BadRequest,
+                "The service had an internal error and could not fulfil the request completly. Please report this error and make sure that you attach the information that you find in the response body.", 
+                typeof(FulcrumError))
+        {
+        }
+    }
+}
