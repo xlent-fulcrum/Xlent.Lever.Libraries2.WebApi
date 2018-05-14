6a27df74e8ed502957ebf6b80f4997851f5a10cc
Lars Lindberg <lars.lindberg@xlent.se>
2018-05-14 13:17:43 +0200

    4.12.1 Added missing Crud class for RestClientHelper.

--- src/Libraries2.WebApi/RestClientHelper/RestClientManyToOneComplete.cs
+++ src/Libraries2.WebApi/RestClientHelper/RestClientManyToOneComplete.cs
@@ -13,7 +13,49 @@ namespace Xlent.Lever.Libraries2.WebApi.RestClientHelper
     /// <summary>
     /// Convenience client for making REST calls
     /// </summary>
-    public class RestClientManyToOneComplete<TManyModel, TId> : RestClientCrud<TManyModel, TId>, IManyToOneComplete<TManyModel, TId>
+    public class RestClientManyToOneComplete<TManyModel, TId> : RestClientManyToOneComplete<TManyModel, TManyModel, TId>,
+        IManyToOneComplete<TManyModel, TId>
+    {
+        /// <summary></summary>
+        /// <param name="baseUri">The base URL that all HTTP calls methods will refer to.</param>
+        /// <param name="parentName">The name of the sub path that is the parent of the children. (Singularis)</param>
+        /// <param name="childrenName">The name of the sub path that are the children. (Pluralis)</param>
+        /// <param name="withLogging">Should logging handlers be used in outbound pipe?</param>
+        public RestClientManyToOneComplete(string baseUri, string parentName = "Parent",
+            string childrenName = "Children", bool withLogging = true)
+            : base(baseUri, parentName, childrenName, withLogging)
+        {
+        }
+
+        /// <summary></summary>
+        /// <param name="baseUri">The base URL that all HTTP calls methods will refer to.</param>
+        /// <param name="parentName">The name of the sub path that is the parent of the children. (Singularis)</param>
+        /// <param name="childrenName">The name of the sub path that are the children. (Pluralis)</param>
+        /// <param name="credentials">The credentials used when making the HTTP calls.</param>
+        /// <param name="withLogging">Should logging handlers be used in outbound pipe?</param>
+        public RestClientManyToOneComplete(string baseUri, ServiceClientCredentials credentials,
+            string parentName = "Parent", string childrenName = "Children", bool withLogging = true)
+            : base(baseUri, credentials, parentName, childrenName, withLogging)
+        {
+        }
+
+        /// <summary></summary>
+        /// <param name="baseUri">The base URL that all HTTP calls methods will refer to.</param>
+        /// <param name="parentName">The name of the sub path that is the parent of the children. (Singularis)</param>
+        /// <param name="childrenName">The name of the sub path that are the children. (Pluralis)</param>
+        /// <param name="authenticationToken">The token used when making the HTTP calls.</param>
+        /// <param name="withLogging">Should logging handlers be used in outbound pipe?</param>
+        public RestClientManyToOneComplete(string baseUri, AuthenticationToken authenticationToken,
+            string parentName = "Parent", string childrenName = "Children", bool withLogging = true)
+            : base(baseUri, authenticationToken, parentName, childrenName, withLogging)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Convenience client for making REST calls
+    /// </summary>
+    public class RestClientManyToOneComplete<TManyModelCreate, TManyModel, TId> : RestClientCrud<TManyModelCreate, TManyModel, TId>, IManyToOneComplete<TManyModelCreate, TManyModel, TId> where TManyModel : TManyModelCreate
     {
         /// <summary>
         /// The name of the sub path that is the parent of the children. (Singularis)
@@ -108,7 +150,7 @@ namespace Xlent.Lever.Libraries2.WebApi.RestClientHelper
         protected virtual async Task<IEnumerable<TManyModel>> SimulateReadChildrenAsync(TId parentId, int limit = int.MaxValue, CancellationToken token = default(CancellationToken))
         {
             InternalContract.RequireGreaterThan(0, limit, nameof(limit));
-            var items = new PageEnvelopeEnumerableAsync<TManyModel>((offset,t) => ReadChildrenWithPagingAsync(parentId, offset, null, t), token);
+            var items = new PageEnvelopeEnumerableAsync<TManyModel>((offset, t) => ReadChildrenWithPagingAsync(parentId, offset, null, t), token);
             var list = new List<TManyModel>();
             var count = 0;
             using (var enumerator = items.GetEnumerator())
